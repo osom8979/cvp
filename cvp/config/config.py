d119a820942bed37ab3f1756a587643abb096db6
zer0 <osom8979@gmail.com>
2024-10-12 13:55:14 +0900

    Rename: ConfigDumper -> DefaultDumper

--- cvp/config/config.py
+++ cvp/config/config.py
@@ -2,10 +2,10 @@
 
 from dataclasses import dataclass, field
 from os import PathLike
-from typing import Final, List, Optional, Union
+from typing import List, Union
 
 from type_serialize import deserialize, serialize
-from yaml import Dumper, dump, full_load
+from yaml import dump, full_load
 
 from cvp.config.sections.appearance import AppearanceConfig
 from cvp.config.sections.concurrency import ConcurrencyConfig
@@ -31,21 +31,7 @@ from cvp.config.sections.wsd import WsdConfig, WsdManagerConfig
 from cvp.config.sections.wsdl import WsdlConfig
 from cvp.inspect.member import get_public_instance_attributes
 from cvp.itertools.find_index import find_index
-from cvp.types import override
-
-ROOT_INDENT: Final[int] = 0
-
-
-class ConfigDumper(Dumper):
-    @override
-    def increase_indent(self, flow=False, indentless=False):
-        return super().increase_indent(flow, False)
-
-    @override
-    def write_line_break(self, data: Optional[str] = None) -> None:
-        super().write_line_break(data)
-        if self.indent == ROOT_INDENT:
-            super().write_line_break(data)
+from cvp.yaml.dumpers import DefaultDumper
 
 
 @dataclass
@@ -110,7 +96,7 @@ class Config:
         return self.wsds.pop(index)
 
     def dumps_yaml(self, encoding="utf-8") -> bytes:
-        return dump(serialize(self), Dumper=ConfigDumper).encode(encoding)
+        return dump(serialize(self), Dumper=DefaultDumper).encode(encoding)
 
     def loads_yaml(self, data: bytes) -> None:
         result = deserialize(full_load(data), type(self))
