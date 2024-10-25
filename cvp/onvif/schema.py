e2f5263533b34038c8ccc55fbbd731efdecac8b6
zer0 <osom8979@gmail.com>
2024-10-25 17:12:54 +0900

    Update OnvifSchema

--- cvp/onvif/schema.py
+++ cvp/onvif/schema.py
@@ -1,36 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from typing import Final
+from typing import Final, List
 
-from zeep.xsd.schema import Schema
+from lxml import etree
 
+from cvp.wsdl.cache import ZeepFileCache
 from cvp.wsdl.transport import create_transport_with_package_asset
 
-COMMON_XSD: Final[str] = "http://www.onvif.org/ver10/schema/common.xsd"
-METADATASTREAM_XSD: Final[str] = "http://www.onvif.org/ver10/schema/metadatastream.xsd"
 ONVIF_XSD: Final[str] = "http://www.onvif.org/ver10/schema/onvif.xsd"
 
 
 class OnvifSchema:
-    def __init__(
-        self,
-        common=COMMON_XSD,
-        metadatastream=METADATASTREAM_XSD,
-        onvif=ONVIF_XSD,
-    ):
+    def __init__(self, location=ONVIF_XSD):
         transport = create_transport_with_package_asset()
-        self._common = Schema(transport=transport, location=common)
-        self._metadatastream = Schema(transport=transport, location=metadatastream)
-        self._onvif = Schema(transport=transport, location=onvif)
+        assert isinstance(transport.cache, ZeepFileCache)
+
+        onvif_data = transport.cache.get(location)
+        assert onvif_data is not None
+
+        self._element = etree.XML(onvif_data)
+        assert self._element is not None
+
+        self._xs = {"xs": "http://www.w3.org/2001/XMLSchema"}
 
     @property
-    def common(self):
-        return self._common
+    def element(self):
+        return self._element
+
+    def xpath(self, xpath: str):
+        return self._element.xpath(xpath, namespaces=self._xs)
 
     @property
-    def metadatastream(self):
-        return self._metadatastream
+    def simple_types(self):
+        return self.xpath("//xs:simpleType")
 
     @property
-    def onvif(self):
-        return self._onvif
+    def complex_types(self):
+        return self.xpath("//xs:complexType")
+
+    @property
+    def simple_type_names(self):
+        return [e.get("name") for e in self.simple_types]
+
+    @property
+    def complex_type_names(self):
+        return [e.get("name") for e in self.complex_types]
+
+    def get_enumerations(self, simple_type: str) -> List[str]:
+        xpath = f"//xs:simpleType[@name='{simple_type}']/xs:restriction/xs:enumeration"
+        enumerations = self.xpath(xpath)
+        return [enum.get("value") for enum in enumerations]
--- tester/onvif/test_schema.py
+++ tester/onvif/test_schema.py
@@ -9,15 +9,17 @@ class SchemaTestCase(TestCase):
     def setUp(self):
         self.schema = OnvifSchema()
 
-    def test_onvif(self):
-        namespaces = self.schema.onvif.namespaces
-        documents = [d for d in self.schema.onvif.documents]
-        elements = [e for e in self.schema.onvif.elements]
-        types = [t for t in self.schema.onvif.types]
-        self.assertIsInstance(namespaces, list)
-        self.assertIsInstance(documents, list)
-        self.assertIsInstance(elements, list)
-        self.assertIsInstance(types, list)
+    def test_simple_types(self):
+        simple_type_names = self.schema.simple_type_names
+        self.assertTrue(simple_type_names)
+
+    def test_complex_type(self):
+        complex_type_names = self.schema.complex_type_names
+        self.assertTrue(complex_type_names)
+
+    def test_stream_type(self):
+        types = self.schema.get_enumerations("StreamType")
+        self.assertSetEqual({"RTP-Unicast", "RTP-Multicast"}, set(types))
 
 
 if __name__ == "__main__":
