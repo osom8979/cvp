cd4d79df8e9ca5cfcf611e219beed6a2b095b5b5
zer0 <osom8979@gmail.com>
2024-10-30 22:03:11 +0900

    Upgrade and test XsdSchema

--- cvp/wsdl/schema.py
+++ cvp/wsdl/schema.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict
-from typing import Dict, Final, List
+from typing import Dict, Final, List, Optional, TypeAlias, Union
 from urllib.parse import urljoin, urlparse
 
 from lxml import etree
+from lxml.etree import QName
 from zeep.transports import Transport
 
 # noinspection PyProtectedMember
-_EtreeElement = etree._Element
-_NamespaceKey = str
-_UrlKey = str
-_XmlNs = str
+from lxml.etree import _Element as _EtreeElement  # isort:skip
+
+_NamespaceKey: TypeAlias = str
+_UrlKey: TypeAlias = str
+_XmlNs: TypeAlias = str
 
 XSD_NS: Final[str] = "xs"
 XSD_URL: Final[str] = "http://www.w3.org/2001/XMLSchema"
@@ -20,54 +22,62 @@ XSD_URL: Final[str] = "http://www.w3.org/2001/XMLSchema"
 class XsdSchema:
     _loaded: Dict[_UrlKey, _EtreeElement]
     _prefixes: Dict[_XmlNs, _NamespaceKey]
-    _simple_types: Dict[str, _EtreeElement]
-    _complex_types: Dict[str, _EtreeElement]
+    _simple_types: Dict[QName, _EtreeElement]
+    _complex_types: Dict[QName, _EtreeElement]
+    _types: Dict[QName, _EtreeElement]
+    _names: Dict[str, _EtreeElement]
 
-    def __init__(self, location: str, transport: Transport):
+    def __init__(
+        self,
+        location: str,
+        transport: Transport,
+        *,
+        root: Optional[_EtreeElement] = None,
+    ):
         self._loaded = dict()
         self._prefixes = dict()
         self._simple_types = dict()
         self._complex_types = dict()
         self._types = dict()
+        self._names = dict()
 
         self._tree = OrderedDict[_NamespaceKey, _EtreeElement]()
         self._root_location = location
         self._transport = transport
         self._parser = etree.XMLParser(remove_comments=True)
 
-        data = self._transport.load(location)
-        if not data:
-            raise ValueError("There is no schema data")
+        if root is None:
+            data = self._transport.load(location)
+            if not data:
+                raise ValueError("There is no schema data")
+            root = etree.XML(data, self._parser)
 
-        self._root = etree.XML(data, self._parser)
-        if self._root is None:
+        if root is None:
             raise ValueError("Schema element could not be read")
 
-        target_namespace = self._root.attrib.get("targetNamespace")
-        self._tree[target_namespace] = self._root
+        target_namespace = root.attrib.get("targetNamespace")
+        assert isinstance(target_namespace, str)
+        assert target_namespace
 
-        self._process_element(self._root)
-
-        for prefix, namespace in self._root.nsmap.items():
-            assert isinstance(prefix, _XmlNs)
-            assert isinstance(namespace, _NamespaceKey)
-            self._prefixes[prefix] = namespace
-
-        for node in self._tree.values():
-            for e in self.get_simple_types(node):
-                name = e.get("name")
-                assert isinstance(name, str)
-                self._simple_types[name] = e
-                self._types[name] = e
-            for e in self.get_complex_types(node):
-                name = e.get("name")
-                assert isinstance(name, str)
-                self._complex_types[name] = e
-                self._types[name] = e
+        self._root = root
+        self._root_namespace = target_namespace
+        self._process_element(root)
+        self.insert(target_namespace, root)
 
     @property
-    def root(self):
+    def root(self) -> _EtreeElement:
         return self._root
+
+    @property
+    def root_namespace(self):
+        return self._root_namespace
+
+    @property
+    def root_prefix(self):
+        for prefix, namespace in self._prefixes.items():
+            if namespace == self._root_namespace:
+                return prefix
+        raise IndexError(f"Not found root prefix: '{self._root_namespace}'")
 
     @property
     def namespaces(self):
@@ -89,16 +99,20 @@ class XsdSchema:
     def types(self):
         return self._types
 
-    @staticmethod
-    def get_simple_types(node: _EtreeElement, ns=XSD_URL):
-        return node.iterchildren(f"{{{ns}}}simpleType")
+    @property
+    def names(self):
+        return self._names
 
     @staticmethod
-    def get_complex_types(node: _EtreeElement, ns=XSD_URL):
-        return node.iterchildren(f"{{{ns}}}complexType")
+    def _get_simple_types(node: _EtreeElement, *, ns=XSD_URL) -> List[_EtreeElement]:
+        return list(node.iterchildren(f"{{{ns}}}simpleType"))
 
     @staticmethod
-    def is_url(url: str) -> bool:
+    def _get_complex_types(node: _EtreeElement, *, ns=XSD_URL) -> List[_EtreeElement]:
+        return list(node.iterchildren(f"{{{ns}}}complexType"))
+
+    @staticmethod
+    def _is_url(url: str) -> bool:
         try:
             result = urlparse(url)
             return all((result.scheme, result.netloc))
@@ -106,7 +120,7 @@ class XsdSchema:
             return False
 
     def resolve_url(self, location: str) -> str:
-        if self.is_url(location):
+        if self._is_url(location):
             return location
         else:
             return urljoin(urljoin(self._root_location, "."), location)
@@ -121,11 +135,12 @@ class XsdSchema:
         self._loaded[url] = node
         return node
 
-    def _process_element(self, parent: _EtreeElement):
-        self._process_includes(parent)
-        self._process_imports(parent)
+    def _process_element(self, parent: _EtreeElement, *, ns=XSD_URL):
+        self._process_includes(parent, ns=ns)
+        self._process_imports(parent, ns=ns)
+        self._process_nsmap(parent, ns=ns)
 
-    def _process_includes(self, parent: _EtreeElement, ns=XSD_URL):
+    def _process_includes(self, parent: _EtreeElement, *, ns=XSD_URL):
         for node in parent.iterchildren(f"{{{ns}}}include"):
             assert isinstance(node, _EtreeElement)
             schema_location = node.attrib.get("schemaLocation")
@@ -145,7 +160,7 @@ class XsdSchema:
                 assert isinstance(elem, _EtreeElement)
                 parent.append(elem)
 
-    def _process_imports(self, parent: _EtreeElement, ns=XSD_URL):
+    def _process_imports(self, parent: _EtreeElement, *, ns=XSD_URL):
         for node in parent.iterchildren(f"{{{ns}}}import"):
             assert isinstance(node, _EtreeElement)
             namespace = node.attrib.get("namespace")
@@ -162,14 +177,88 @@ class XsdSchema:
             if child.tag != f"{{{ns}}}schema":
                 continue
 
-            self._tree[namespace] = child
-            self._process_element(child)
+            self.insert(namespace, child)
+
+    def _process_nsmap(self, parent: _EtreeElement, *, ns=XSD_URL):
+        if parent.tag != f"{{{ns}}}schema":
+            return
+
+        for prefix, namespace in parent.nsmap.items():
+            if prefix is None:
+                prefix = self._gen_prefix()
+            assert isinstance(prefix, _XmlNs)
+            assert isinstance(namespace, _NamespaceKey)
+            self._prefixes[prefix] = namespace
+
+    def _gen_prefix(self, *, prefix="ns", index=0) -> _XmlNs:
+        candidate_prefix = f"{prefix}{index}"
+        for ns in self._prefixes.keys():
+            if candidate_prefix == ns:
+                index += 1
+                candidate_prefix = f"{prefix}{index}"
+        return candidate_prefix
+
+    def insert(self, namespace: str, node: _EtreeElement) -> None:
+        self._tree[namespace] = node
+
+        for e in self._get_simple_types(node):
+            name = e.get("name")
+            if not name:
+                continue
+            assert isinstance(name, str)
+            qname = QName(f"{{{namespace}}}{name}")
+            self._simple_types[qname] = e
+            self._types[qname] = e
+            self._names[qname.localname] = e
+
+        for e in self._get_complex_types(node):
+            name = e.get("name")
+            if not name:
+                continue
+            assert isinstance(name, str)
+            qname = QName(f"{{{namespace}}}{name}")
+            self._complex_types[qname] = e
+            self._types[qname] = e
+            self._names[qname.localname] = e
+
+    def get_type(self, name: Union[str, QName]) -> _EtreeElement:
+        if isinstance(name, QName):
+            qname = name
+        elif isinstance(name, str):
+            try:
+                qname = QName(name)
+            except ValueError:
+                if name.find(":") == -1:
+                    raise
+                prefix, localname = name.split(":", 1)
+                qname = QName(f"{{{self._prefixes[prefix]}}}{localname}")
+        else:
+            raise TypeError(f"Unexpected type: {type(name).__name__}")
+
+        if not qname.namespace:
+            return self._names[qname.localname]
+
+        if not self._is_url(qname.namespace):
+            qname.namespace = self._prefixes[qname.namespace]
+
+        return self._types[qname]
+
+    def __getitem__(self, item):
+        return self.get_type(item)
+
+    def get_root_type(self, localname: str) -> _EtreeElement:
+        return self.get_type(f"{{{self._root_namespace}}}{localname}")
 
     @staticmethod
-    def enumerations_xpath(name: str, xs=XSD_NS) -> str:
-        return f"//{xs}:simpleType[@name='{name}']/{xs}:restriction/{xs}:enumeration"
+    def get_restriction(node: _EtreeElement, *, ns=XSD_URL) -> List[_EtreeElement]:
+        return list(node.iterchildren(f"{{{ns}}}restriction"))
 
-    def get_enumerations(self, simple_type: str, xs=XSD_NS, xsns=XSD_URL) -> List[str]:
-        xpath = self.enumerations_xpath(simple_type, xs)
-        enumerations = self.root.xpath(xpath, namespaces={xs: xsns})
-        return [enum.get("value") for enum in enumerations]
+    @staticmethod
+    def get_enumeration(node: _EtreeElement, *, ns=XSD_URL) -> List[_EtreeElement]:
+        restriction = XsdSchema.get_restriction(node, ns=ns)[0]
+        return list(restriction.iterchildren(f"{{{ns}}}enumeration"))
+
+    @staticmethod
+    def get_enumeration_values(node: _EtreeElement, *, ns=XSD_URL) -> List[str]:
+        enumerations = XsdSchema.get_enumeration(node, ns=ns)
+        return list(e.get("value") for e in enumerations)
--- tester/onvif/test_schema.py
+++ tester/onvif/test_schema.py
@@ -9,16 +9,38 @@ class SchemaTestCase(TestCase):
     def setUp(self):
         self.schema = OnvifSchema()
 
+    def test_default(self):
+        prefix = self.schema.root_prefix
+        namespace = self.schema.root_namespace
+        self.assertEqual("tt", prefix)
+        self.assertEqual("http://www.onvif.org/ver10/schema", namespace)
+
     def test_types(self):
         simple_types = self.schema.simple_types
         complex_types = self.schema.complex_types
         types = self.schema.types
+
+        self.assertEqual(109, len(simple_types))
+        self.assertEqual(521, len(complex_types))
         self.assertEqual(len(types), len(simple_types) + len(complex_types))
 
     def test_stream_type(self):
-        stream_types = self.schema.get_enumerations("StreamType")
-        self.assertIsNotNone(stream_types)
-        self.assertSetEqual({"RTP-Unicast", "RTP-Multicast"}, set(stream_types))
+        stream_type0 = self.schema["StreamType"]
+        stream_type1 = self.schema[f"{self.schema.root_prefix}:StreamType"]
+        stream_type2 = self.schema[f"{{{self.schema.root_namespace}}}StreamType"]
+        stream_type3 = self.schema.get_root_type("StreamType")
+        self.assertEqual(stream_type0, stream_type1)
+        self.assertEqual(stream_type1, stream_type2)
+        self.assertEqual(stream_type2, stream_type3)
+
+    def test_get_enumeration_values(self):
+        stream_type = self.schema.get_type("StreamType")
+        values = self.schema.get_enumeration_values(stream_type)
+        self.assertSetEqual({"RTP-Unicast", "RTP-Multicast"}, set(values))
+
+    def test_time_zone(self):
+        time_zone = self.schema.get_type("TimeZone")
+        self.assertEqual("TimeZone", time_zone.attrib.get("name"))
 
 
 if __name__ == "__main__":
