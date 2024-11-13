d826cc9b70be68117ee9e39f4db0b5767e349bcc
zer0 <osom8979@gmail.com>
2024-11-13 23:25:57 +0900

    Create PLANES

---
+++ cvp/strings/unicode/planes.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+from typing import Final, NamedTuple, Sequence
+
+PLANE_INDEX_MIN: Final[int] = 0
+PLANE_INDEX_MAX: Final[int] = 16
+
+
+class UnicodePlane(NamedTuple):
+    idx: int
+    long: str
+    short: str
+    begin: int
+    end: int
+
+    def __repr__(self):
+        return f"Plane {self.idx} {self.short}: U+{self.begin:04X} - U+{self.end:04X}"
+
+
+class UnicodePlaneCategory(NamedTuple):
+    long: str
+    short: str
+    begin: int
+    end: int
+
+    def __repr__(self):
+        return f"{self.long} ({self.short}): U+{self.begin:04X} - U+{self.end:04X}"
+
+    def make_plane(self, idx: int, begin: int, end: int) -> UnicodePlane:
+        assert PLANE_INDEX_MIN <= idx <= PLANE_INDEX_MAX
+        assert self.begin <= begin <= self.end
+        assert self.begin <= end <= self.end
+        assert begin < end
+        return UnicodePlane(idx, self.long, self.short, begin, end)
+
+    def __call__(self, idx: int) -> UnicodePlane:
+        begin = idx * 0x10000
+        end = begin + 0xFFFF
+        return self.make_plane(idx, begin, end)
+
+
+# fmt: off
+BMP = UnicodePlaneCategory("Basic Multilingual Plane", "BMP", 0x0000, 0xFFFF)
+SMP = UnicodePlaneCategory("Supplementary Multilingual Plane", "SMP", 0x10000, 0x1FFFF)
+SIP = UnicodePlaneCategory("Supplementary Ideographic Plane", "SIP", 0x20000, 0x2FFFF)
+TIP = UnicodePlaneCategory("Tertiary Ideographic Plane", "TIP", 0x30000, 0x3FFFF)
+UNASSIGNED = UnicodePlaneCategory("Unassigned", "-", 0x40000, 0xDFFFF)
+SSP = UnicodePlaneCategory("Supplementary Special-purpose Plane", "SSP", 0xE0000, 0xEFFFF)  # noqa: E501
+PUA_A = UnicodePlaneCategory("Private Use Plane A", "PUA-A", 0xF0000, 0xFFFFF)
+PUA_B = UnicodePlaneCategory("Private Use Plane B", "PUA-B", 0x100000, 0x10FFFF)
+# fmt: on
+
+PUA_AB = UnicodePlaneCategory(
+    "Supplementary Private Use Area planes",
+    "SPUA-A/B",
+    PUA_A.begin,
+    PUA_B.end,
+)
+
+PLANE0 = BMP(0)
+PLANE1 = SMP(1)
+PLANE2 = SIP(2)
+PLANE3 = TIP(3)
+PLANE4 = UNASSIGNED(4)
+PLANE5 = UNASSIGNED(5)
+PLANE6 = UNASSIGNED(6)
+PLANE7 = UNASSIGNED(7)
+PLANE8 = UNASSIGNED(8)
+PLANE9 = UNASSIGNED(9)
+PLANE10 = UNASSIGNED(10)
+PLANE11 = UNASSIGNED(11)
+PLANE12 = UNASSIGNED(12)
+PLANE13 = UNASSIGNED(13)
+PLANE14 = SSP(14)
+PLANE15 = PUA_A(15)
+PLANE16 = PUA_B(15)
+
+PLANES: Sequence[UnicodePlane] = (
+    PLANE0,
+    PLANE1,
+    PLANE2,
+    PLANE3,
+    PLANE4,
+    PLANE5,
+    PLANE6,
+    PLANE7,
+    PLANE8,
+    PLANE9,
+    PLANE10,
+    PLANE11,
+    PLANE12,
+    PLANE13,
+    PLANE14,
+    PLANE15,
+    PLANE16,
+)
--- cvp/windows/font/__init__.py
+++ cvp/windows/font/__init__.py
@@ -6,9 +6,17 @@ import imgui
 
 from cvp.config.sections.font import FontManagerConfig
 from cvp.context.context import Context
+from cvp.imgui.begin_child import begin_child
 from cvp.imgui.font_manager import FontItem, FontMapper
 from cvp.imgui.input_text_disabled import input_text_disabled
+from cvp.imgui.item_width import item_width
+from cvp.imgui.slider_float import slider_float
 from cvp.types.override import override
+from cvp.variables import (
+    DEFAULT_API_SELECT_WIDTH,
+    MAX_API_SELECT_WIDTH,
+    MIN_API_SELECT_WIDTH,
+)
 from cvp.widgets.manager import Manager
 
 
@@ -22,6 +30,9 @@ class FontManager(Manager[FontManagerConfig, FontItem]):
             flags=None,
         )
         self._fonts = fonts
+        self._range_select_width = DEFAULT_API_SELECT_WIDTH
+        self._min_range_select_width = MIN_API_SELECT_WIDTH
+        self._max_range_select_width = MAX_API_SELECT_WIDTH
 
     @override
     def get_menus(self) -> Mapping[str, FontItem]:
@@ -31,6 +42,17 @@ class FontManager(Manager[FontManagerConfig, FontItem]):
     def on_process_sidebar_top(self) -> None:
         pass
 
+    def slider_range_select_width(self) -> None:
+        result = slider_float(
+            "## Unicode Range List Width",
+            self._range_select_width,
+            self._min_range_select_width,
+            self._max_range_select_width,
+            "List width (%.3f)",
+        )
+        if result:
+            self._range_select_width = result.value
+
     @override
     def on_menu(self, key: str, item: FontItem) -> None:
         imgui.text("Font information")
@@ -38,3 +60,33 @@ class FontManager(Manager[FontManagerConfig, FontItem]):
 
         input_text_disabled("Font family", item.family)
         input_text_disabled("Font pixel size", str(item.size_pixels))
+
+        with begin_child("Unicode Range List", width=self._range_select_width):
+            with item_width(-1):
+                self.slider_range_select_width()
+
+                # list_box = imgui.begin_list_box(
+                #     "## Unicode Range List Box",
+                #     width=-1,
+                #     height=-1,
+                # )
+                # if list_box.opened:
+                #     with list_box:
+                #         for key in apis.keys():
+                #             if imgui.selectable(key, key == item.select_api)[1]:
+                #                 item.select_api = key
+
+        # import unicodedata
+        # for codepoint in range(0x110000):
+        #     char = chr(codepoint)
+        #     try:
+        #         name = unicodedata.name(char)
+        #     except ValueError:
+        #         name = "No name"
+        #
+        #     category = unicodedata.category(char)
+        #     combining = unicodedata.combining(char)
+        #     bidirectional = unicodedata.bidirectional(char)
+        #
+        #     print(f"U+{codepoint:04X} {char} - Name: {name}, Category: {category}, "
+        #           f"Combining: {combining}, Bidirectional: {bidirectional}")
