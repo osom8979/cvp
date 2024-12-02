6a282e3379ab97841ce021e0de65e1479e34cdf3
zer0 <osom8979@gmail.com>
2024-12-02 09:01:15 +0900

    Create font dirs

--- cvp/assets/__init__.py
+++ cvp/assets/__init__.py
@@ -54,6 +54,10 @@ def get_default_icon_path() -> str:
     return os.path.join(get_icons_dir(), "icon.png")
 
 
+JBM_DIR: Final[str] = "JetBrainsMono"
+MDI_DIR: Final[str] = "MaterialDesignIcons"
+NGC_DIR: Final[str] = "NanumGothicCoding"
+
 FONT_FILENAME_JBM_NL_NFM_R: Final[str] = "JetBrainsMonoNLNerdFontMono-Regular"
 FONT_FILENAME_MDI: Final[str] = "materialdesignicons-webfont"
 FONT_FILENAME_NGC: Final[str] = "NanumGothicCoding"
@@ -68,22 +72,22 @@ FONT_TTF_FILENAME_NGC_B: Final[str] = FONT_FILENAME_NGC_B + _TTF
 
 @lru_cache
 def get_jbm_nl_nfm_r_font_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_TTF_FILENAME_JBM_NL_NFM_R)
+    return os.path.join(get_fonts_dir(), JBM_DIR, FONT_TTF_FILENAME_JBM_NL_NFM_R)
 
 
 @lru_cache
 def get_mdi_font_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_TTF_FILENAME_MDI)
+    return os.path.join(get_fonts_dir(), MDI_DIR, FONT_TTF_FILENAME_MDI)
 
 
 @lru_cache
 def get_ngc_font_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_TTF_FILENAME_NGC)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_TTF_FILENAME_NGC)
 
 
 @lru_cache
 def get_ngc_b_font_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_TTF_FILENAME_NGC_B)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_TTF_FILENAME_NGC_B)
 
 
 _RANGES: Final[str] = CODEPOINT_RANGES_EXTENSION
@@ -95,22 +99,22 @@ FONT_RANGES_FILENAME_NGC_B: Final[str] = FONT_FILENAME_NGC_B + _RANGES
 
 @lru_cache
 def get_jbm_nl_nfm_r_font_ranges_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_RANGES_FILENAME_JBM_NL_NFM_R)
+    return os.path.join(get_fonts_dir(), JBM_DIR, FONT_RANGES_FILENAME_JBM_NL_NFM_R)
 
 
 @lru_cache
 def get_mdi_font_ranges_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_RANGES_FILENAME_MDI)
+    return os.path.join(get_fonts_dir(), MDI_DIR, FONT_RANGES_FILENAME_MDI)
 
 
 @lru_cache
 def get_ngc_font_ranges_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_RANGES_FILENAME_NGC)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_RANGES_FILENAME_NGC)
 
 
 @lru_cache
 def get_ngc_b_font_ranges_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_RANGES_FILENAME_NGC_B)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_RANGES_FILENAME_NGC_B)
 
 
 _GLYPHS: Final[str] = CODEPOINT_GLYPHS_EXTENSION
@@ -122,19 +126,19 @@ FONT_GLYPHS_FILENAME_NGC_B: Final[str] = FONT_FILENAME_NGC_B + _GLYPHS
 
 @lru_cache
 def get_jbm_nl_nfm_r_font_glyphs_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_GLYPHS_FILENAME_JBM_NL_NFM_R)
+    return os.path.join(get_fonts_dir(), JBM_DIR, FONT_GLYPHS_FILENAME_JBM_NL_NFM_R)
 
 
 @lru_cache
 def get_mdi_font_glyphs_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_GLYPHS_FILENAME_MDI)
+    return os.path.join(get_fonts_dir(), MDI_DIR, FONT_GLYPHS_FILENAME_MDI)
 
 
 @lru_cache
 def get_ngc_font_glyphs_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_GLYPHS_FILENAME_NGC)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_GLYPHS_FILENAME_NGC)
 
 
 @lru_cache
 def get_ngc_b_font_glyphs_path() -> str:
-    return os.path.join(get_fonts_dir(), FONT_GLYPHS_FILENAME_NGC_B)
+    return os.path.join(get_fonts_dir(), NGC_DIR, FONT_GLYPHS_FILENAME_NGC_B)
